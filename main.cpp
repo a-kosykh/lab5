31cc31026937747351a297fdcaff647e500af9ff
Aleksandr Kosykh <kik9399@mail.ru>
2016-11-25 02:08:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,27 +3,29 @@ using namespace std;
 
 int sum(int x, int y)
 {
-    return x+y;
+	return x + y;
+}
+int sub(int x, int y) {
+	return x - y;
 }
 
-int main(int argc, char *argv[]) {
-    int x=2;
-    int y=3;
-    int a=0;
-    /*if (sscanf(argv[1], "%d", &x) < 1)
-    {
-        printf("Ошибка, неверный формат первого входного параметра. Для завершения нажмите любую клавишу\n");
-        system("pause");
-        return 1;
-    }
-
-    if (sscanf(argv[2], "%d", &y) < 1)
-    {
-        printf("Ошибка, неверный формат первого входного параметра. Для завершения нажмите любую клавишу\n");
-        system("pause");
-        return 1;}
-    */
-     a = sum(x,y);
-    cout << a << endl;
-    return 0;
+int main(int argc, char *argv[]) 
+{
+	int x, y;
+	if (!sscanf_s(argv[1], "%d", &x)) 
+	{
+		printf_s("arg1 should be number\n");
+		system("pause");
+		return 1;
+	}
+	if (!sscanf_s(argv[2], "%d", &y))
+	{
+		printf_s("arg2 should be number\n");
+		system("pause");
+		return 1;
+	}
+	cout << "Summa: " << sum(x, y) << endl;
+	cout << "Vychitanie: " << sub(x, y) << endl;
+	system("pause");
+	return 0;
 }
